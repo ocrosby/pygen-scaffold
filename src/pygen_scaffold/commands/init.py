1ab832f552f81bea6b10f8a854067239b5c69741
Omar Crosby <omar.crosby@gmail.com>
2022-12-22 09:16:27 -0500

    feat(init): updating types for init command

--- src/pygen_scaffold/commands/init.py
+++ src/pygen_scaffold/commands/init.py
@@ -1,7 +1,7 @@
 import click
 
 @click.command()
-@click.option('-t', '--type', type=click.Choice(["project", "module"]), help="type of the new project")
+@click.option('-t', '--type', type=click.Choice(["api", "cmd", "module"]), help="type of the new project")
 def cli():
     """Initialize a project in the current directory."""
     click.echo(f'Initializing project ...')
